aa8031329a66bf40e4faf7f86277095888466e99
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-08-14 18:43:24 -0300

    Ajustes nos arquivos

--- src/componentes/Sidebar/index.jsx
+++ src/componentes/Sidebar/index.jsx
@@ -1,41 +1,43 @@
-import Logo from ".assets/logo.svg";
-import feed from ".assets/feed.svg";
-import perfil from ".assets/perfil.svg";
-import sobreNos from ".assets/info.svg";
-import logout from ".assets/logout.svg";
+import Logo from "./assets/Logo.svg";
+import Feed from "./assets/feed.svg";
+import Account from "./assets/account_circle.svg";
+import Info from "./assets/info.svg";
+import Logout from "./assets/logout.svg";
 export default function Sidebar() {
   return (
     <aside>
-      <img src={Logo} alt="logo do codeconnect" />
+      <img src={Logo} alt="Logo do CodeConnect" />
       <nav>
-        <ul>
-            <li>
-                <a href="#">Publicar</a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src={feed} alt="" />
-                    <span>Feed</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src={perfil} alt="" />
-                    <span>Perfil</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src={sobreNos} alt="" />
-                    <span>Sobre Nós</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <img src={logout} alt="" />
-                    <span>Sair</span>
-                </a>
-            </li>
+        <ul className="lista-sidebar">
+          <li>
+            <a href="#" className="item__link-publicacao">
+              Publicar
+            </a>
+          </li>
+          <li>
+            <a href="#" className="item__link item__link--ativo">
+              <img src={Feed} alt="" />
+              <span>Feed</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="item__link">
+              <img src={Account} alt="" />
+              <span>Perfil</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="item__link">
+              <img src={Info} alt="" />
+              <span>Sobre nós</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className="item__link">
+              <img src={Logout} alt="" />
+              <span>Sair</span>
+            </a>
+          </li>
         </ul>
       </nav>
     </aside>
