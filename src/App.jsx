ccda849862bd3ff76fdfc20ec07ec16d2c853721
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-08-14 21:04:03 -0300

    Campo de digitação

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
-import Sidebar from './componentes/Sidebar'
+import "./App.css";
+import BarraDePesquisa from "./componentes/BarraDePesquisa";
+import Sidebar from "./componentes/Sidebar";
 
 function App() {
-
   return (
-    <Sidebar />
-  )
+    <>
+      <Sidebar />
+      <BarraDePesquisa />
+    </>
+  );
 }
 
-export default App
+export default App;
